6358e41fe2f45e9ffd7b4c15da271b97ef48fe75
Martin Kedmenec <martin.kedmenec@outlook.com>
2024-09-29 19:54:56 +0200

    Fix #4. Add basic components

---
+++ components/server/toolbar-display.tsx
@@ -0,0 +1,8 @@
+import type { FC, ReactElement } from "react";
+import ToolbarMenu from "@/components/client/toolbar-menu";
+
+const ToolbarDisplay: FC = (): ReactElement | null => {
+  return <ToolbarMenu />;
+};
+
+export default ToolbarDisplay;
